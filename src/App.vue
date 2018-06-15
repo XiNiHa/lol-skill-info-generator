352f06b740cb021fee1b8266eb3bbb8e2a48d0c3
anesin1109 <anesin1109@naver.com>
2018-06-15 18:55:42 +0900

    Make Things Work

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <app-skill-generator @change="processOutput"/>
+    <textarea id="output"></textarea>
+  </div>
+</template>
+
+<script>
+import appSkillGenerator from './components/SkillGenerator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    appSkillGenerator
+  },
+  methods: {
+    processOutput () {
+      setTimeout(() => {
+        let wrapperObject = document.getElementsByClassName('skills-wrapper')[0]
+        document.getElementById('output').value = wrapperObject.innerHTML.replace(/ data-v-[^\s]+=""/g, '')
+      }, 10)
+    }
+  }
+}
+</script>
+
+<style>
+/* only for app */
+#app {
+  display: flex;
+}
+
+#skill-generator {
+  width: 700px
+}
+
+#output {
+  flex-grow: 1;
+}
+
+/* common */
+.skills-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.skills-master-order {
+  display: flex;
+  align-items: center;
+  margin: 5px;
+}
+
+.skills-master-order span {
+  margin: 10px;
+}
+
+.skill-icon {
+  width: 60px;
+  height: 60px;
+}
+
+.skill-wrapper {
+  border: 1px solid #c9c9c9
+}
+
+.skill-letter {
+  text-align: center;
+}
+
+.skills-select-table {
+  margin: 5px;
+  border: 1px solid #c9c9c9;
+  text-align: center;
+  border-collapse: collapse
+}
+
+.skills-table-header {
+  background: #fff;
+  border: 1px solid #999;
+  width: 25px;
+  height: 25px;
+}
+
+.skills-table-cell {
+  background: #f9f9f9;
+  border: 1px solid #d9d9d9;
+  width: 25px;
+  height: 25px;
+}
+
+.skills-table-cell-active {
+  background: rgb(59, 183, 255);
+}
+</style>
---
+++ src/components/SkillGenerator.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="skill-generator">
+    <select v-model="champion">
+      <option v-for="(champ, key, i) in champions" :key="i" :value="key">{{ champ.name }}</option>
+    </select>
+    <div v-if="currentChampion !== undefined" class="skills-wrapper">
+      <div v-if="!skillsMasterOrder.includes(-1)" class="skills-master-order">
+        <div class="skill-wrapper">
+          <img :src="'https://ddragon.leagueoflegends.com/cdn/8.12.1/img/spell/' + currentChampion.spells[skillsMasterOrder[0]].image.full" class="skill-icon">
+          <div class="skill-letter">{{ skillNumToLetter(skillsMasterOrder[0]) }}</div>
+        </div>
+        <span>></span>
+        <div class="skill-wrapper">
+          <img :src="'https://ddragon.leagueoflegends.com/cdn/8.12.1/img/spell/' + currentChampion.spells[skillsMasterOrder[1]].image.full" class="skill-icon">
+          <div class="skill-letter">{{ skillNumToLetter(skillsMasterOrder[1]) }}</div>
+        </div>
+        <span>></span>
+        <div class="skill-wrapper">
+          <img :src="'https://ddragon.leagueoflegends.com/cdn/8.12.1/img/spell/' + currentChampion.spells[skillsMasterOrder[2]].image.full" class="skill-icon">
+          <div class="skill-letter">{{ skillNumToLetter(skillsMasterOrder[2]) }}</div>
+        </div>
+        <span>></span>
+        <div class="skill-wrapper">
+          <img :src="'https://ddragon.leagueoflegends.com/cdn/8.12.1/img/spell/' + currentChampion.spells[skillsMasterOrder[3]].image.full" class="skill-icon">
+          <div class="skill-letter">{{ skillNumToLetter(skillsMasterOrder[3]) }}</div>
+        </div>
+      </div>
+      <div>
+        <table class="skills-select-table">
+          <thead>
+            <th v-for="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]" :key="i" class="skills-table-header">{{ i }}</th>
+          </thead>
+          <tbody>
+            <tr v-for="i in [0, 1, 2, 3]" :key="i">
+              <td v-for="j in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]" :key="j" :class="{'skills-table-cell': true, 'skills-table-cell-active': skillsSelectOrder[j] === i}" @click="$emit('change'); Vue.set(skillsSelectOrder, j, i); evalMasterOrder()">{{ skillNumToLetter(i) }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Axios from 'axios'
+
+export default {
+  data () {
+    return {
+      Vue,
+      skillsMasterOrder: [-1, -1, -1, -1],
+      skillsSelectOrder: [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+      champion: '',
+      currentChampion: undefined,
+      champions: []
+    }
+  },
+  watch: {
+    champion () {
+      Axios.get('https://ddragon.leagueoflegends.com/cdn/8.12.1/data/ko_KR/champion/<name>.json'.replace(/<name>/, this.champion)).then(res => {
+        this.currentChampion = res.data.data[this.champion]
+      })
+    }
+  },
+  mounted () {
+    Axios.get('https://ddragon.leagueoflegends.com/cdn/8.12.1/data/ko_KR/champion.json').then(res => {
+      this.champions = res.data.data
+    })
+  },
+  methods: {
+    skillNumToLetter (num) {
+      switch (num) {
+        case 0:
+          return 'Q'
+        case 1:
+          return 'W'
+        case 2:
+          return 'E'
+        case 3:
+          return 'R'
+      }
+    },
+    evalMasterOrder () {
+      if (this.skillsSelectOrder.includes(-1)) {
+        return
+      }
+      let currentOrder = 0
+      let counter = [0, 0, 0, 0]
+      for (let i of this.skillsSelectOrder) {
+        counter[i]++
+        if (counter[i] === this.currentChampion.spells[i].maxrank) {
+          this.skillsMasterOrder[currentOrder] = i
+          currentOrder++
+        }
+      }
+      if (this.skillsSelectOrder[17] !== this.skillsMasterOrder[3]) {
+        this.skillsMasterOrder[3] = this.skillsSelectOrder[17]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
